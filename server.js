a530ff6625c8b6535b7c705e65c4fc95abf4a5a1
howardlin1218 <howardlin526@gmail.com>
2026-04-10 13:17:29 -0700

    update

--- server.js
+++ server.js
@@ -45,4 +45,4 @@ app.post('/api/transcribe', upload.single('file'), async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Backend live on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Backend live on port ${PORT}`));
